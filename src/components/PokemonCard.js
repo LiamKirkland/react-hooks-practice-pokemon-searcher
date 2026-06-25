196d225ef438592569e4b3697729466504a3b55f
LiamKirkland <liam.b.kirkland@gmail.com>
2026-06-25 10:38:00 -0400

    Display PokeCards

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({poke}) {
+  const [sprite, setSprite] = useState("front")
+  const {id, name, hp, sprites} = poke
+
+  function handleClick() {
+    setSprite(sprite === "front" ? "back" : "front")
+  }
+  console.log(poke)
   return (
     <Card>
-      <div>
+      <div onClick={handleClick}>
         <div className="image">
-          <img alt="oh no!" />
+          <img alt="oh no!" src={sprites[sprite]}/>
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {hp} hp
           </span>
         </div>
       </div>
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,12 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({pokeList}) {
+  const pokemonToDisplay = pokeList
+
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      {pokemonToDisplay.map(poke => <PokemonCard poke={poke} key={poke.id}/>)}
     </Card.Group>
   );
 }
